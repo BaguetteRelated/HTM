d06d4e559ef7eb1490e85952bc54ba2cebd9c957
Fabien Morrow <fabien.morrow@protonmail.com>
2022-09-17 11:29:11 +0200

    basic css formatting

--- my-app/components/Layout.js
+++ my-app/components/Layout.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import Header from './Header'
 
-
+import styles from "../styles/Layout.module.css"
 
 export default function Layout({ children }) {
     return (
         <>
-            <div>
+            <div className={styles.appContainer}>
                 <Header />
                 <main>{children}</main>
             </div>
