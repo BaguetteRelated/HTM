f356427c2c77f81f581ad7e89ee58cb1abafb1c2
Fabien Morrow <fabien.morrow@protonmail.com>
2022-09-17 23:59:42 +0200

    added Koficoin.sol

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -8,7 +8,7 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const Lock = await hre.ethers.getContractFactory("Hello");
+  const Lock = await hre.ethers.getContractFactory("KofiCoin");
   const lock = await Lock.deploy();
 
   await lock.deployed();
