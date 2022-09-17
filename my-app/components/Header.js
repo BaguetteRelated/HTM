5612e4f463b408dfbefbe529ccc20a78a277971f
Fabien Morrow <fabien.morrow@protonmail.com>
2022-09-17 12:43:11 +0200

    menu styling

--- my-app/components/Header.js
+++ my-app/components/Header.js
@@ -1,50 +1,56 @@
 import React from "react"
-
+import Link from 'next/link'
 import styles from '../styles/Header.module.css'
 
 import '@rainbow-me/rainbowkit/styles.css';
 
-import {
-    getDefaultWallets,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import {
-    chain,
-    configureChains,
-    createClient,
-    WagmiConfig,
-} from 'wagmi';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import { publicProvider } from 'wagmi/providers/public';
-
-const { chains, provider } = configureChains(
-    [chain.polygonMumbai],
-    [
-        jsonRpcProvider({
-            priority: 0,
-            rpc: (chain) => ({
-                http: `https://polygon-mumbai.gateway.pokt.network/v1/lb/63256464708ade00392c6754`,
-            }),
-        })
-    ]
-);
-
-const { connectors } = getDefaultWallets({
-    appName: 'My RainbowKit App',
-    chains
-});
-
-const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider
-})
-
-
-export default function Header() {
+function Home() {
     return (
-        <div>
-            header
+        <div className={styles.homeContainer}>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
         </div>
     )
 }
+
+function Menu() {
+    return (
+        <div className={styles.menuContainer}>
+            <div className={styles.menuItem}>
+                <Link href="/Projects">
+                    <a >My projects</a>
+                </Link>
+            </div>
+            <div className={styles.menuItem}>
+                <Link href="/Supports">
+                    <a >My supports</a>
+                </Link>
+            </div>
+            <div className={styles.menuItemEnd}>
+                <Link href="/Profile">
+                    <a >My profile</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+function Search() {
+    return (
+        <div className={styles.searchContainer}>
+            <input class={styles.searchInput} type="text" placeholder="Search" />
+        </div>
+    )
+}
+
+export default function Header() {
+    return (
+        <div className={styles.headerContainer}>
+            <Home />
+            <Menu />
+            <Search />
+        </div>
+    )
+}
+
