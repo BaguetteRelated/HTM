a0689819e5cff43da464e8a83a28eb5ba15ed746
Kraibonaut <79799749+Kraibonaut@users.noreply.github.com>
2022-09-18 11:36:49 +0200

    Add files via upload

---
+++ hardhat/contracts/Reward20.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract TokenReward is ERC20Capped, Ownable{
+    constructor(uint256 cap) ERC20("TokenReward", "TKR") ERC20Capped(cap){
+}
+
+    function issueToken() public{
+        _mint(msg.sender, 2*10** decimals());
+    }
+}
