04995eedd66dd47e441d67c7d2a7d0c0906645a1
Kraibonaut <79799749+Kraibonaut@users.noreply.github.com>
2022-09-18 12:14:15 +0200

    Update Koficoin.sol

--- hardhat/contracts/Koficoin.sol
+++ hardhat/contracts/Koficoin.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
@@ -8,12 +8,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KofiCoin is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("KofiCoin", "KF") {
-        _mint(msg.sender, 100 * 10**decimals());
+contract KofiCoin is ERC20, ERC20Burnable, Ownable{
+    constructor() ERC20("KofiCoin", "KF"){
+        _mint(msg.sender, 100 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function issueToken() public{
+        _mint(msg.sender, 2*10** decimals());
     }
 }
